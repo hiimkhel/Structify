24fb9caf2d8f14e50cf0f730534860c1949c0b62
ydrhan <kellyydrhan.alojepan@wvsu.edu.ph>
2025-05-07 01:33:16 +0800

    Modified Menu to utilize keyboard input

--- main.cpp
+++ main.cpp
@@ -1,20 +1,69 @@
 // main.cpp
 #include "structify.h"
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 
+using namespace std;
+
+string UserMenuOptions[] = {"Guest (Student)", "Admin (Instructor)", "EXIT"};
+int selected = 0;
+int optionCount = sizeof(UserMenuOptions) / sizeof(UserMenuOptions[0]);
+
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+};
+
+void displayMenu(int highlight){
+    system("cls");
+    cout << "===== Welcome to Structify =====\n";
+    for(int i = 0; i < optionCount; i++){
+        if(i == highlight){
+            setColor(15);
+            cout << " > " << UserMenuOptions[i] << endl;
+        }
+        else{
+            setColor(8); 
+            cout << "  " << UserMenuOptions[i] << endl;
+        }
+        
+        
+    }
+    setColor(7); 
+    cout << "\n\nUse UP/DOWN arrow keys to navigate, ENTER to select.\n\n";
+}
 int main() {
-    clearScreen();
-    std::cout << "===== Welcome to Structify =====\n";
-    int userType;
-    std::cout << "Select User Type:\n";
-    std::cout << "[1] Guest (Student)\n";
-    std::cout << "[2] Admin (Instructor)\n";
-    std::cout << "Enter choice: ";
-    std::cin >> userType;
+    
+    int key;
 
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
+    while(true){
+        displayMenu(selected);
+        key = _getch();
+
+        if(key == 224){
+            key = _getch();
+            if(key == 72 && selected > 0)
+                selected--;
+            else if(key == 80 && selected < optionCount - 1)
+                selected++;
+        }
+        else if( key == 13){
+            system("cls");
+            cout << "You selected: " << UserMenuOptions[selected] << endl;
+            setColor(7); 
+            if (UserMenuOptions[selected] == "EXIT") break;
+            system("pause");
+        }
+
+        
+    }
+
+
+
+    /*
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
 
     User* user = nullptr;
 
@@ -23,7 +72,7 @@ int main() {
     } else if (userType == 2) {
         user = new Admin();
     } else {
-        std::cout << "Invalid user type. Exiting...\n";
+        cout << "Invalid user type. Exiting...\n";
         return 1;
     }
 
@@ -31,6 +80,6 @@ int main() {
     user->dashboard();
 
     delete user;
-    std::cout << "Thank you for using Structify!\n";
+    cout << "Thank you for using Structify!\n"; */
     return 0;
 }
